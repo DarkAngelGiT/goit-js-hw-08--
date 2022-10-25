af92540934ff6bc28b61d61e5f5b14122890fc26
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-10-25 22:11:18 +0300

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  button: document.querySelector('.feedback-form button'),
+}
+
+// console.log(refs.form)
+// console.log(refs.input)
+// console.log(refs.textarea)
+// console.log(refs.button)
 
